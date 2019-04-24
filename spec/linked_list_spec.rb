7c161bb84d7d563d4d329da2556f2adeb1be35cf
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-24 16:51:55 -0600

    Iteration 1

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,8 +2,17 @@ require_relative 'spec_helper'
 
 RSpec.describe LinkedList, type: :class do
 
-  it 'tests that it is working' do
+  it 'it adds a single node' do
 
+    list = LinkedList.new
+    expect(list.head).to eq(nil)
+
+    list.append("doop")
+
+    expect(list.head.data).to eq("doop")
+    expect(list.head.next_node).to eq(nil)
+    expect(list.count).to eq(1)
+    expect(list.to_string).to eq("doop")
   end
 
 end
