d2cfdb80be2c5c9459f2c6ab6314dbe7b04a1f07
Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
2019-04-25 16:21:23 -0600

    Update linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -79,26 +79,3 @@ class LinkedList
   end
 
 end
-
-
-
-#
-# def find_at_index(index)
-#   last_spot = @head.next_node
-#   count = 0
-#   until last_spot.next_node.nil? || count == index
-#     last_spot = last_spot.next_node
-#     count += 1
-#   end
-#   last_spot
-# end
-# 0   1   2
-# doo wop bam
-# bop
-#
-# def insert(index, new_node)
-#   existing_node_at_idnex = find_at_index(index)
-#   node_pointing_at_existing = find_at_index(index-1)
-#   node_pointing_at_existing.next_node = new_node
-#   new_node.next_node = existing_node_at_idnex
-# end
